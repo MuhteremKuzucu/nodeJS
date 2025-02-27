ddf9ca329c71d1cced4bf69059def8d3e7fcf967
MuhteremKuzucu <kuzucumuhterem@gmail.com>
2025-02-27 12:12:19 +0100

    up

---
+++ class-notes/14-BlogApi_Mongoose/index.js
@@ -0,0 +1,41 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+
+//Parse data
+app.use(express.json())
+
+//Catch erorr from async
+require('express-async-errors')
+
+//DB Connection
+// const dbConnection = require('./src/dbConnection');
+// dbConnection()
+
+require('./src/dbConnection')()
+
+//Main route
+app.all('/', (req, res) => {
+    res.send('WELCOME TO BLOG API')
+})
+
+// Blog route
+
+app.use(require('./src/routes/blogRouter'))
+
+
+// Catch Errors:
+app.use(require('./src/middlewares/errorHandler'))
+
+/* ------------------------------------------------------- */
+
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
---
+++ class-notes/14-BlogApi_Mongoose/src/routes/blogRouter.js
@@ -0,0 +1,16 @@
+"use strict";
+/* ----------------------------------------------
+   EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------ */
+
+const router = require('express').Router()
+
+const blogCategory = require('../controllers/blogController')
+
+/* ---------------------------------------------- */
+// URL: /blog
+
+router.route('/blog')
+      .get(blogCategory.list)
+
+module.exports = router;
